4931ccc2d02f7307c79821691d88816c8ab90517
rushi7637 <rushikesh.patil@mitaoe.ac.in>
2025-05-24 17:31:12 +0530

    chnages in App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
-     import Sidebar from './components/Sidebar';
-     import Header from './components/Header';
-     import DashboardMainContent from './components/DashboardMainContent';
-     import './App.css';
+import Sidebar from './components/Sidebar';
+import Header from './components/Header';
+import DashboardMainContent from './components/DashboardMainContent';
+import './App.css';
 
-     const App = () => {
-       return (
-         <div className="app-container">
-           <h1></h1> {/* Test to ensure something renders */}
-           <Sidebar />
-           <div className="main-content">
-             <Header />
-             <DashboardMainContent />
-           </div>
-         </div>
-       );
-     };
+const App = () => {
+  return (
+    <div className="app-container">
+      {/* <h1>Healthcare Dashboard</h1> */}
+      <Sidebar />
+      <div className="main-content">
+        <Header />
+        <DashboardMainContent />
+      </div>
+    </div>
+  );
+};
 
-     export default App;
+export default App;
